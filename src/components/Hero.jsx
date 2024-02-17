33b956f9fb15c91e205355f82d1b9e7d0a0599ad
JK-mern <Jkmerndev@gmail.com>
2024-02-17 22:37:06 +0530

    added icons of social media on hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faSquareGithub,
+  faSquareXTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
 function Hero() {
   return (
-    <div className="h-4/6" id="Home">
-      <div className="max-w-6xl mx-auto mt-2 md:mt-24 md:mb-28 flex  flex-col md:flex-row justify-around   items-center ">
+    <div className="h-4/6 " id="Home">
+      <div className="max-w-6xl  mx-auto mt-2 md:mt-24 md:mb-28 flex  flex-col md:flex-row justify-around   items-center ">
         <div className="">
-          <h3 className="text-xl md:text-2xl  text-center   md:text-left text-slate-500 ml-3">Hai, I'm, </h3>
+          <h3 className="text-xl md:text-2xl  text-center   md:text-left text-slate-500 ml-3">
+            Hai, I'm,{" "}
+          </h3>
           <h2 className="text-4xl md:text-5xl text-center  mt-3 text-slate-950 font-extrabold md:text-left">
             Jayakrishnan S
           </h2>
@@ -25,13 +33,37 @@ function Hero() {
               repeat={Infinity}
             />
           </div>
+          <div className="my-6 flex gap-4  ">
+            <a href="" target="_blank">
+              {" "}
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                size="2xl"
+                style={{ color: "#8b8e92" }}
+                className=" cursor-pointer"
+              />
+            </a>
+            <a href="" target="_blank">
+              <FontAwesomeIcon
+                icon={faSquareXTwitter}
+                size="2xl"
+                style={{ color: "#8b8e92" }}
+                className=" cursor-pointer"
+              />
+            </a>
+            <a href="https://github.com/JK-mern" target="_blank">
+              {" "}
+              <FontAwesomeIcon
+                icon={faSquareGithub}
+                size="2xl"
+                style={{ color: "#8b8e92" }}
+                className=" cursor-pointer"
+              />
+            </a>
+          </div>
         </div>
         <div className="flex mt-10 justify-center items-center">
-          <img
-            src="photo2.jpg"
-            alt=""
-            className=" w-fit md:w-72 rounded-xl"
-          />
+          <img src="photo2.jpg" alt="" className=" w-fit md:w-72 rounded-xl" />
         </div>
       </div>
     </div>
